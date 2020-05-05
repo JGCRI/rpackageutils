56f4da31ceb3bcb0e6f7d35859dae4bb3b650700
crvernon <chrisrvernon@gmail.com>
2020-05-05 17:23:04 -0400

    tests for download

--- tests/testthat/test_download_unpack.R
+++ tests/testthat/test_download_unpack.R
@@ -10,3 +10,13 @@ test_that("filename_from_url() returns expected:", {
 
   testthat::expect_identical(fn, 'test.csv')
 })
+
+
+test_that("download_unpack_zip() returns error:", {
+
+  error_string <- "cannot open URL 'b'"
+  warning_string <- "URL 'http://b/': status was 'Couldn't resolve host name'"
+
+  testthat::expect_error(download_unpack_zip('a', 'b'), error_string, ignore.case = TRUE)
+
+})
