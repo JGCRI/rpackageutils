eea8d5aee9776bb40a9632d84e41ea657243146c
crvernon <chrisrvernon@gmail.com>
2020-05-26 12:02:03 -0400

    update tests to be more comprehensive

--- R/constants.R
+++ R/constants.R
@@ -1,3 +1,3 @@
 #' A keyed list containing links to download remote data for each package version in the structure:
 #' list(version_1 = c(data_url_1, data_url_n), #' version_n = c(data_url_1, data_url_n))
-DATA_VERSION_URL_LIST <- list('0.1.0' = list('https://zenodo.org/record/3856417/files/test_with-header.csv?download=1', 'CSV', NULL))
+DATA_VERSION_URL_LIST <- list('0.1.0' = list('https://zenodo.org/record/3856417/files/test.zip?download=1', 'ZIP', NULL))
