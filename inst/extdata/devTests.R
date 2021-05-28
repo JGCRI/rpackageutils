6ee0f493c453b9d883b1d62ffbeac5132dadf177
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-05-28 13:50:58 -0400

    Updating download_unpack_unzip to check dir before downloading

--- inst/extdata/devTests.R
+++ inst/extdata/devTests.R
@@ -1,6 +1,8 @@
 library(rpackageutils)
 
+
 rpackageutils::download_unpack_zip(
+  #url="https://zenodo.org/record/4404738/files/mengqi-z/demeter-v1.1.0-wild2020-ArgentinaNexus.zip?download=1",
   url="https://zenodo.org/record/4641500/files/IDBNexus_gcam5p3_HadGEM2-ES_rcp8p5.zip?download=1",
   data_directory = getwd()) -> datadir
 
